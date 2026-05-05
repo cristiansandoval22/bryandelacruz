415cbb407ddd92d715e884f98bca654367f66e2d
Cristian Sandoval <cristiansandoval2202@gmail.com>
2026-05-05 17:14:59 -0500

    midelware corregido

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
-import { defineMiddleware } from "astro:middleware";
+// import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware((context, next) => {
-  if (import.meta.env.DEV) {
-    return next();
-  }
+// export const onRequest = defineMiddleware((context, next) => {
+//   if (import.meta.env.DEV) {
+//     return next();
+//   }
 
-  if (context.url.pathname.startsWith("/studio")) {
-    const token = context.cookies.get("admin");
+//   if (context.url.pathname.startsWith("/studio")) {
+//     const token = context.cookies.get("admin");
 
-    if (!token) {
-      return context.redirect("/");
-    }
-  }
+//     if (!token) {
+//       return context.redirect("/");
+//     }
+//   }
 
-  return next();
-});
+//   return next();
+// });
 
 
 
